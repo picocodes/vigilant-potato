7cff9583d1910f9e1ecda8c6ad76d35904e51d11
picocodes <picocodes@gmail.com>
2019-04-07 13:31:15 +0300

    Integrated to the WordPress REST API

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,18 @@ const request = require('request');
 const router = new Router(); //router.get|put|post|patch|delete|del
 
 //Load template files
+
+//Blog list
+var blog_template = e => e;
+fs.readFile('dist/blog.html', 'utf8',
+    function(err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            blog_template = handlebars.compile(data);
+        }
+    });
+
 //Posts
 var post_template = e => e;
 fs.readFile('dist/post.html', 'utf8',
@@ -59,6 +71,18 @@ request('http://localhost/wordpress/wp-json/wp/v2/pages?per_page=100', function(
     }
 });
 
+//Load authors
+var authors = db.addCollection('authors', {
+    unique: ['id']
+});
+request('http://localhost/wordpress/wp-json/wp/v2/users?per_page=100', function(error, response, body) {
+    if (!error) {
+        authors.insert(JSON.parse(body));
+    } else {
+        console.log('Cant get users error:', error);
+    }
+});
+
 
 //Bundled js and css files
 router.get('/main.(.*)', async ctx => {
@@ -88,28 +112,33 @@ router.get('/:slug', (ctx, next) => {
     //Posts
     let post = posts.by("slug", ctx.params.slug.toLowerCase());
     if (post) {
+        let date = new Date(post.modified_gmt);
+        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         post.current_year = new Date().getFullYear();
+        post.by = authors.by('id', post.author).name;
+        post.modified_gmt = date.toLocaleDateString("en-US", options);
         ctx.body = post_template(post);
+        return
     }
 
     //Pages
     let page = pages.by("slug", ctx.params.slug.toLowerCase());
     if (page) {
         page.current_year = new Date().getFullYear();
-        ctx.body = post_template(post);
+        ctx.body = page_template(page);
+        return
     }
 
+    //Nothing to do. Move on to the next handler
     next();
-    return;
-
 });
 
 //Homepage
 router.get('/', (ctx, next) => {
-    // the parsed body will store in ctx.request.body
-    // if nothing was parsed, body will be an empty object {}
-    //ctx.body = ctx.request.body;
-    ctx.body = 'This is the homepage';
+    ctx.body = blog_template({
+        current_year: new Date().getFullYear(),
+        posts: posts.find()
+    });
 });
 
 //Compare, Specs
